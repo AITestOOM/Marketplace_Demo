55617ccf3332723a7746f8ec00d9b5a7bcc4ec5a
matus-kovac <133961635+matus-kovac@users.noreply.github.com>
2025-06-19 10:20:08 +0200

    init src

---
+++ src/Marketplace/Program.cs
@@ -0,0 +1,35 @@
+using Marketplace.Components;
+using MudBlazor.Services;
+
+namespace Marketplace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddMudServices();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
